92018419a32ce729dbdfb815b9da59ecc311390b
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-04-24 15:23:17 +0200

    #5 added support for filter_spec (wagtail < 5) and filter_specs (wagtail >= 5) attributes :/

--- wagtail_srcset/templatetags/wagtail_srcset_tags.py
+++ wagtail_srcset/templatetags/wagtail_srcset_tags.py
@@ -2,7 +2,7 @@ import types
 
 from django import template
 from django.conf import settings
-from wagtail.images.templatetags.wagtailimages_tags import image
+from wagtail.images.templatetags.wagtailimages_tags import image as image_tag
 
 register = template.Library()
 
@@ -33,7 +33,7 @@ def monkeypatch_wagtail_as_syntax(image_node):
 
 @register.tag(name="srcset_image")
 def srcset_image(parser, token):
-    image_node = image(parser, token)
+    image_node = image_tag(parser, token)
     image_node.attrs["srcset"] = SrcSet(image_node)
     image_node = monkeypatch_wagtail_as_syntax(image_node)
     return image_node
@@ -52,7 +52,7 @@ class SrcSet:
 
     Entries in the srcset attribute have the same syntax as normal
     wagtail image tag filter specs, although it wouldn't make much
-    sence to use fill-80x80 or scale-50 inside srcset. At the moment
+    sense to use fill-80x80 or scale-50 inside srcset. At the moment
     width and jpegquality are the only supported operations.
     """
 
@@ -64,11 +64,16 @@ class SrcSet:
         self.image_node = image_node
         # need to set this inside __init__
         self.srcset_attribute = self.image_node.attrs.get("srcset", None)
+        self.resolved_image = None
 
-    def get_allowed_operations(self, filter_spec):
+    def get_allowed_operations(self, filter_specs):
+        """
+        Gets a list of filter_specs and returns a list of allowed
+        operations from this list and a set of used operations.
+        """
         operations = []
         used_operations = set()
-        for operation in filter_spec.split("|"):
+        for operation in filter_specs:
             name = operation.split("-")[0]
             already_used = name in used_operations
             operation_allowed = name in self.ALLOWED_OPERATIONS
@@ -77,14 +82,19 @@ class SrcSet:
                 used_operations.add(name)
         return operations, used_operations
 
-    def filter_specs_from_srcset(self, srcset):
+    @staticmethod
+    def filter_specs_from_srcset(srcset):
         srcset = srcset.strip('"').strip("'")
         return srcset.split(" ")
 
     def merge_filter_specs(self, srcset_filter_specs, operations_from_tag):
+        """
+        Merges the filter specs from the srcset attribute with the
+        operations from the tag.
+        """
         filter_specs = []
         for filter_spec in srcset_filter_specs:
-            operations, used = self.get_allowed_operations(filter_spec)
+            operations, used = self.get_allowed_operations([filter_spec])
             # inherit operations from tag if allowed and not already used
             # by the srcset attribute
             for operation in operations_from_tag:
@@ -117,6 +127,13 @@ class SrcSet:
         return filter_specs
 
     def get_srcset_filter_specs(self, image):
+        """
+        Returns the filter specs that should be used for the srcset.
+          - If the srcset attribute is set, use that
+          - Otherwise, use the DEFAULT_SRCSET_RENDITIONS setting if set
+          - Otherwise, calculate the renditions based on the image
+          - Otherwise, use the DEFAULT_WIDTHS setting
+        """
         if self.srcset_attribute is not None:
             return self.filter_specs_from_srcset(self.srcset_attribute.token)
         if hasattr(settings, "DEFAULT_SRCSET_RENDITIONS"):
@@ -125,19 +142,39 @@ class SrcSet:
             return self.calc_filter_specs(image)
         return self.DEFAULT_SRCSET_RENDITIONS
 
+    @property
+    def image_node_filter_specs(self):
+        """
+        Wagtail 5.0 changed the way filter specs are stored on the image node
+        from a string to a list.
+        """
+        if hasattr(self.image_node, "filter_specs"):
+            return self.image_node.filter_specs
+        else:
+            return self.image_node.filter_spec.split("|")
+
     def get_merged_filter_specs(self, image):
+        """
+        Merge the filter specs from the srcset attribute and the filter_specs
+        set on the image node which is passed into this method.
+        """
         srcset_filter_specs = self.get_srcset_filter_specs(image)
-        operations_from_tag, _ = self.get_allowed_operations(
-            self.image_node.filter_spec
-        )
+        image_node_filter_specs = self.image_node_filter_specs
+        # filter operations from tag that should be used in srcset, too
+        operations_from_tag, _ = self.get_allowed_operations(image_node_filter_specs)
         return self.merge_filter_specs(srcset_filter_specs, operations_from_tag)
 
     def resolve(self, context):
+        """
+        Resolve the image node and return a srcset string.
+        """
         self.resolved_image = image = self.image_node.image_expr.resolve(context)
         out_renditions = []
+        # create out_renditions from filter specs
         for filter_spec in self.get_merged_filter_specs(image):
             rendered_image = image.get_rendition(filter_spec)
             out_renditions.append(f"{rendered_image.url} {rendered_image.width}w")
+        # create a srcset string from out_renditions
         srcset_string = ", ".join(out_renditions)
         return srcset_string
 
