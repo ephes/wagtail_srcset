3e50dc1f8669f04ea61b34d23cf6de4d35b8df32
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-11-18 23:53:58 +0100

    generate thumbnails in different sizes and compare manually to have a way to debug raw html directly

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import MainView
 
 urlpatterns = [
     path("", MainView.as_view(), name="main"),
+    path("manual/", TemplateView.as_view(template_name="manual.html"), name="manual"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
